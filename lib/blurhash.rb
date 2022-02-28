cb004da05892d85cdf77b4c488121787e5d31884
Takeshi Umeda <noel.yoshiba@gmail.com>
2022-02-28 09:08:09 +0900

    Fix missing `,` (#16)

--- lib/blurhash.rb
+++ lib/blurhash.rb
@@ -35,7 +35,7 @@ module Blurhash
       U V W X Y Z a b c d
       e f g h i j k l m n
       o p q r s t u v w x
-      y z # $ % * +  - .
+      y z # $ % * + , - .
       : ; = ? @ [ ] ^ _ {
       | } ~
     ).freeze
