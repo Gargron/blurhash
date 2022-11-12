84e52266202b85b9189fc3070530a5855c277ed7
Sebastian Wiedenroth <wiedi@frubar.net>
2022-11-12 23:39:44 +0100

    Use portable integer type

--- lib/blurhash.rb
+++ lib/blurhash.rb
@@ -5,7 +5,7 @@ require 'ffi'
 
 module Blurhash
   def self.encode(width, height, pixels, x_comp: 4, y_comp: 3)
-    FFI::MemoryPointer.new(:u_int8_t, pixels.size) do |p|
+    FFI::MemoryPointer.new(:uint8, pixels.size) do |p|
       p.write_array_of_uint8(pixels)
       return Unstable.blurHashForPixels(x_comp, y_comp, width, height, p, width * 3)
     end
