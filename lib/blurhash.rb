45096e24f2cc313cdd3575f16d45ca1fdc7cfef8
Aaron Patterson <tenderlove@ruby-lang.org>
2023-01-03 17:41:01 -0800

    Use Array#pack to build pixel buffer
    We can use `Array#pack` to build the underlying pixel buffer required
    for performing the blurhash.  This removes one dependency on FFI
    methods.

--- lib/blurhash.rb
+++ lib/blurhash.rb
@@ -5,10 +5,8 @@ require 'ffi'
 
 module Blurhash
   def self.encode(width, height, pixels, x_comp: 4, y_comp: 3)
-    FFI::MemoryPointer.new(:u_int8_t, pixels.size) do |p|
-      p.write_array_of_uint8(pixels)
-      return Unstable.blurHashForPixels(x_comp, y_comp, width, height, p, width * 3)
-    end
+    p = pixels.pack("C#{pixels.size}")
+    return Unstable.blurHashForPixels(x_comp, y_comp, width, height, p, width * 3)
   end
 
   def self.components(str)
