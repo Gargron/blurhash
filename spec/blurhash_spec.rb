3d8a39c9edd4817dbabf58e80a38cc4c19c80f54
Christian Schmidt <github@chsc.dk>
2023-07-31 20:53:40 +0200

    Check pixels array size (#26)

--- spec/blurhash_spec.rb
+++ spec/blurhash_spec.rb
@@ -8,6 +8,10 @@ RSpec.describe Blurhash do
       pixels = File.read(File.join(__dir__, 'fixtures', 'test.bin')).unpack('C*')
       expect(Blurhash.encode(204, 204, pixels)).to eq 'LFE.@D9F01_2%L%MIVD*9Goe-;WB'
     end
+
+    it 'raises if pixels array has wrong size' do
+      expect { Blurhash.encode(204, 204, [0, 1, 2]) }.to raise_error(RuntimeError, 'Pixels array has wrong size')
+    end
   end
 
   describe '.components' do
